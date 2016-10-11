692d90ff64e1b6876a34900cb58b764a628d3bfa
Cedric Fontin <cfontin@maelstrom-research.org>
2016-10-11 17:34:32 -0400

    MK-1001 - can now customize validationMessage 'minimumChecked' in schemaform's definition

--- dist/sf-checkboxgroup.js
+++ dist/sf-checkboxgroup.js
@@ -1,20 +1,26 @@
 angular.module("sfCheckboxgroupTemplates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/templates/checkboxgroup-template.html","<div class=\"checkboxgroup schema-form-checkboxgroup {{form.HtmlClass}}\">\n	<div ng-controller=\"schemaFormCheckboxgroupController\">\n		<div class=\"has-error\" ng-show=\"showMessage\">\n			<span class=\"help-block\">{{message}}</span>\n		</div>\n	</div>\n</div>");}]);
 angular.module('sfCheckboxgroup', ['schemaForm', 'sfCheckboxgroupTemplates'])
-	.config(['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfBuilderProvider', 
-		function (schemaFormProvider, schemaFormDecoratorsProvider, sfBuilderProvider) {
-			schemaFormDecoratorsProvider.defineAddOn(
-        'bootstrapDecorator',
-        'checkboxgroup',
-        'src/templates/checkboxgroup-template.html',
-        sfBuilderProvider.stdBuilders
+  .config(['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfBuilderProvider',
+    function (schemaFormProvider, schemaFormDecoratorsProvider, sfBuilderProvider) {
+      schemaFormDecoratorsProvider.defineAddOn(
+          'bootstrapDecorator',
+          'checkboxgroup',
+          'src/templates/checkboxgroup-template.html',
+          sfBuilderProvider.stdBuilders
       );
-		}])
+    }])
 
 	.controller('schemaFormCheckboxgroupController', ['$scope', function($scope) {
 		var keys = $scope.form.items.map(function (val) { return val.key[0]; });
     var min = $scope.form.minChecked ? parseInt($scope.form.minChecked) : 0;
+
+    var validationMessage = $scope.$parent.form.validationMessage;
     $scope.showMessage = false;
-    $scope.message = 'At least ' + min + ' selection(s) expected.';
+    if (validationMessage && validationMessage.minimumChecked) {
+      $scope.message = validationMessage.minimumChecked;
+    } else {
+      $scope.message = 'At least ' + min + ' selection(s) expected.';
+    }
 
     var models = [];
 
